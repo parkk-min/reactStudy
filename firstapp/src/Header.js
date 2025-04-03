289e5f07641f17e728e1f331cd5da48962733fe6
reroad-hub <mingeu0821@naver.com>
2025-04-03 22:48:34 +0900

    da

--- firstapp/src/Header.js
+++ firstapp/src/Header.js
@@ -1,10 +1,18 @@
-export default function Header(props) { //{title : "REACT" onChangeMode : (0=>{alert("Header")})}
-    return (
-        <header>
-            <h1><a href='/' onClick={(e) => {
-                e.preventDefault();
-                props.onChangeMode();
-            }}>{props.title}</a></h1>
-        </header>
-    );
+export default function Header(props) {
+  //{title : "REACT" onChangeMode : (0=>{alert("Header")})}
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
 }
--- test_exam/src/App.js
+++ test_exam/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Link } from "react-router-dom";
+
+const contents = [
+  { id: 1, title: "Html", body: "Html is..." },
+  { id: 2, title: "Css", body: "Css is..." },
+  { id: 3, title: "Java", body: "Java is..." },
+];
+
+function Home() {
+  return (
+    <>
+      <h2>Home</h2> Home is...
+    </>
+  );
+}
+
+function Topics() {
+  return (
+    <>
+      <h2>Topics</h2> Topics is...
+    </>
+  );
+}
+
+function Contact() {
+  return (
+    <>
+      <h2>Contact</h2> Contact is...
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>React Router Dom example</h1>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/topics">Topics</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+
+    </ul>
+      <Routes>
+        <Route path="/"><Home/></Route>
+        <Route path="/topics"><Topics/></Route>
+        <Route path="/contact"><Contact/></Route>
+      </Routes>
+    </>
   );
 }
 
